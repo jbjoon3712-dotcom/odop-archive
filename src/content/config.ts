a824b25216e8b20f167c1bd904426f4a477a1e6a
정병준 <jebi@jeongbyeongjun-ui-MacBookPro.local>
2026-01-27 19:36:01 +0900

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,14 @@
+// src/content/config.ts
 import { defineCollection, z } from "astro:content";
 
-export const collections = {
-  odop: defineCollection({
-    type: "content",
-    schema: z.object({
-      day: z.string(),
-      subject: z.string().optional(),
-      tools: z.array(z.string()).nullable().optional(),
-      fonts: z.array(z.string()).nullable().optional(),
-
-    }),
+const odop = defineCollection({
+  type: "content",
+  schema: z.object({
+    day: z.string().optional(),
+    subject: z.string().optional(),
+    tools: z.array(z.string()).optional(),
+    fonts: z.array(z.string()).optional(),
   }),
-};
+});
 
+export const collections = { odop };
