b7303e542582cdb9230cddc5255fab5abbf97df0
정병준 <jebi@jeongbyeongjun-ui-MacBookPro.local>
2026-02-07 18:15:44 +0900

    update

--- src/lib/posters.js
+++ src/lib/posters.js
@@ -5,7 +5,7 @@ export const startDate = new Date(2025, 7, 15, 12, 0, 0);
 const descMap = {
 };
 
-export const posters = Array.from({ length: 176
+export const posters = Array.from({ length: 177
 
  }).map((_, i) => {
   const no = i + 1;
